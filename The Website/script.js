a17b2c353558979b6fae7b187b4a0c5d8f749dce
Bratislav Ranđelović <31524550+bracar@users.noreply.github.com>
2018-02-19 10:42:07 +0100

    Add comments in js and  img alt

--- The Website/script.js
+++ The Website/script.js
@@ -1,3 +1,5 @@
 window.addEventListener('click', ()=>{
+  // Show picture when user clicks anywhere on screen
   document.getElementById("secret-picture").style.visibility = "visible";
+  // TODO Show picture only when user types secret word
 })
