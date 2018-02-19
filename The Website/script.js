871e64961056756ab9740b66c38810630f1ffe20
mills3 <mills827@gmail.com>
2018-02-19 19:39:51 +0000

    Added function to compare keys pressed to secret string

--- The Website/script.js
+++ The Website/script.js
@@ -3,3 +3,29 @@ window.addEventListener('click', ()=>{
   document.getElementById("secret-picture").style.visibility = "visible";
   // TODO Show picture only when user types secret word
 })
+
+let secretArray = ['r', 'o', 'm', 'c', 'h', 'y'];
+let userArray = [];
+
+const secretCompare = (e) => {
+  //Get the key pressed
+  let key = String.fromCharCode(e.keyCode);
+  userArray.push(key.toLowerCase());
+
+  //Slice the secretArray and compare to userArray on every keypress
+  let partialArray = secretArray.slice(0, userArray.length);
+
+  //If key pressed doesn't match sliced secretArray, clear userArray
+  if(userArray.join('') !== partialArray.join('')) {
+    userArray = [];
+  }
+
+  //If user types secret code, do awesome stuff!
+  //Clear userArray so they can keep adding unicorns
+  if(userArray.join('') === secretArray.join('')) {
+    cornify_add();
+    userArray = [];
+  }
+}
+
+document.addEventListener('keydown', (e) => secretCompare(e));
