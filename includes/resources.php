49e6158623d12edff983c72bc26141205cc878b7
Tee Wen Seng <twenseng7227@gmail.com>
2018-08-01 19:11:41 +0800

    Changed the interface

---
+++ includes/resources.php
@@ -0,0 +1,29 @@
+<div id="resources" class="main">
+  <div class="mainContent" >
+  Resources
+  </div>
+  <div class="mainMiddle">
+    <div class="mainText" style="font-size:0.7em">
+      <ul>
+        <li>
+          <a href="https://help.github.com/articles/fork-a-repo/">Fork a repo</a>
+        </li>
+        <li>
+          <a href="https://help.github.com/articles/cloning-a-repository/">Cloning a repo</a>
+        </li>
+        <li>
+          <a href="https://help.github.com/articles/about-pull-requests/">Pull Requests</a>
+        </li>
+        <li>
+          <a href="https://devdocs.io">DevDocs</a>
+        </li>
+        <li>
+          <a href="https://github.com/JABedford/Front-End-Bible">the Front End Bible</a>
+        </li>
+        <li>
+          <a href="https://guide.freecodecamp.org/">fCC Library</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
